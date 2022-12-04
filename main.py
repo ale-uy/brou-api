8fdf5550b568a0d29b775a34867a15e5cac3e9f4
Ale <46233654+lasm-uy@users.noreply.github.com>
2022-12-03 23:05:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -27,4 +27,4 @@ def api_brou_2():
         return 'No funciona el scraper, error: {}'.format(e)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("app", host="0.0.0.0", port=8080, reload=True)
